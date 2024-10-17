128be93208f42727c288ec01ea408d8c0da11a76
rakibul-islam-mehedi <rakibulislammehedi4@gmail.com>
2024-10-17 09:23:07 +0600

    Basic Stateful Counter App

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,47 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-import 'CRUDApp.dart';
+void main() {
+  runApp(MyApp());
+}
 
-void main(){
-  runApp(CRUDApp());
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Counter App",
+      home: CounterApp(),
+    );
+  }
+}
+
+class CounterApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return CounterAppUI();
+  }
+}
+
+class CounterAppUI extends State<CounterApp> {
+  int count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Counter App'),
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: Center(
+        child: Text('Counting Number: ${count}'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            count++;
+          });
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
 }
