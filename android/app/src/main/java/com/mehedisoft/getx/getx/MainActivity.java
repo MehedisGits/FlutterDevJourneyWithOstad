dc057c88b7a1575b286cc6d6db600f563c2da69b
mehedi <rakibulislammehedi4@gmail.com>
2025-01-19 09:59:34 +0600

    GetX

---
+++ android/app/src/main/java/com/mehedisoft/getx/getx/MainActivity.java
@@ -0,0 +1,6 @@
+package com.mehedisoft.getx.getx;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+import 'package:getx/views/homePage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'All about GetX',
+      home: const HomePage(),
+      initialBinding: ControllerBinder(),
+    );
+  }
+}
+
+class ControllerBinder extends Bindings {
+  @override
+  void dependencies() {}
+}
